e17bd73918981bff5ef76052f3472bd018288223
Mauricio Marin <marinmauricio66@gmail.com>
2019-01-11 01:59:13 -0800

    Add sign up api

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // DB config
 const db = require("./config/keys").mongoURI;
 
