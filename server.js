8f206b1114863d35b6c830752bfcbd70199f4f38
Mauricio Marin <marinmauricio66@gmail.com>
2019-01-10 23:12:00 -0800

    Add connection to database

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to DB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected."))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
 
 const server = app.listen(process.env.PORT || 5000, () =>
-  console.log("server listening on 5000...")
+  console.log(`server listening on 5000...`)
 );
