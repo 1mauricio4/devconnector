ca20c561e60edf03e6b869ed9997c8a1b2c5b118
Mauricio Marin <marinmauricio66@gmail.com>
2019-01-10 23:41:15 -0800

    Add api routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // DB config
@@ -16,6 +20,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const server = app.listen(process.env.PORT || 5000, () =>
   console.log(`server listening on 5000...`)
