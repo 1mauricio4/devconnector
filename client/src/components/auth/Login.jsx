0304ebb438d800161be8a024631e6f9226b099c5
Mauricio Marin <marinmauricio66@gmail.com>
2019-01-22 11:35:17 -0800

    Add react router to utilize different paths

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h5>Login</h5>
+      </div>
+    );
+  }
+}
