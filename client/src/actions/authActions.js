862561a3263d2d2b3ef3213e21d5d8ee8a05b18a
Mauricio Marin <marinmauricio66@gmail.com>
2019-02-21 21:33:46 -0800

    Add login reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,10 @@
-import { GET_ERRORS } from "./types";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
+import setAuthToken from "../utils/setAuthToken";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import { decode } from "punycode";
 
 // Register User
 export const registerUser = (userData, history) => async dispatch => {
@@ -17,4 +23,51 @@ export const registerUser = (userData, history) => async dispatch => {
       payload: json
     });
   }
+};
+
+// login - Get user token
+export const loginUser = userData => async dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // Set token to auth Header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+
+  // let res = fetch("/api/users/login", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify(userData)
+  // });
+
+  // let json = await res.json();
+
+  // if (!res.ok) {
+  //   dispatch({
+  //     type: GET_ERRORS,
+  //     payload: json
+  //   });
+  // }
+};
+
+// set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
