619564a26032bfe88e351f4dfd2e2484d5802494
Mauricio Marin <marinmauricio66@gmail.com>
2019-03-19 23:16:45 -0700

    Add Profile component and ProfileItem sub-component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -7,7 +7,8 @@ import {
   PROFILE_LOADING,
   GET_ERRORS,
   CLEAR_CURRENT_PROFILE,
-  SET_CURRENT_USER
+  SET_CURRENT_USER,
+  GET_PROFILES
 } from "./types";
 
 // Get current profile
@@ -27,6 +28,25 @@ export const getCurrentProfile = () => dispatch => {
         payload: {}
       })
     );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      });
+    });
 };
 
 // Profile loading
