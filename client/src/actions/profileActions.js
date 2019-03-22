7f4214a3725fa16189eec1cfe78ec86a2fbae9a3
Mauricio Marin <marinmauricio66@gmail.com>
2019-03-21 22:16:12 -0700

    Add profile component with sub components: header, about, cred

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -49,6 +49,25 @@ export const getProfiles = () => dispatch => {
     });
 };
 
+// Get Profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Profile loading
 export const setProfileLoading = () => {
   return {
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import is_Empty from "../../validation/is-empty";
+
+class ProfileAbout extends Component {
+  render() {
+    const { profile } = this.props;
+    const fullNameArr = profile.user.name.trim().split(" ");
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
+
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{fullNameArr[0]}'s Bio</h3>
+            <p className="lead">
+              {is_Empty(profile.bio) ? (
+                <span>{fullNameArr[0]} does not have a bio.</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;
