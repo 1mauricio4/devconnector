6d2e3003f068d47e3b301be71a8291cf365c9af3
Mauricio Marin <marinmauricio66@gmail.com>
2019-03-26 00:27:28 -0700

    Add post components along with redux actions and reducers

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+// components
+import PostForm from "./PostForm";
+import PostFeed from "./PostFeed";
+import Spinner from "../common/Spinner";
+// redux actions
+import { getPosts } from "../../actions/postActions";
+
+class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
+  render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
+
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+              {postContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,41 @@
+import {
+  POST_LOADING,
+  GET_POST,
+  GET_POSTS,
+  ADD_POST,
+  DELETE_POST
+} from "../actions/types";
+
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
+    default:
+      return state;
+  }
+};
